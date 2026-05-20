386409ebca06c1930ce1e5f5d60844b97250c292
Honaco <147415604+Honaco@users.noreply.github.com>
2026-05-20 09:28:32 +0300

    add

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -12,7 +12,7 @@ INSTALLED_APPS = [
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [BASE_DIR / 'templates'], 
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
